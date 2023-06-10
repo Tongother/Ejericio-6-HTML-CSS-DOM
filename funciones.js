0b18d26699b03a934eab973c6db10d3dab6ced51
Gunther Nettel <Nettelgunther@outlook.es>
2023-06-09 20:30:08 -0600

    Tarea terminada v2.1

--- funciones.js
+++ funciones.js
@@ -2,7 +2,7 @@ const botonesComprar = document.querySelectorAll(".btnComprar");
 const ulElements = document.querySelector(".dropdown-menu");
 const avisoNoHayProducto = document.querySelector("#avisoNoHayProducto");
 const btnPagar = document.getElementById("btnPagar");
-let divSeparador, pElement, h6Element, cantidadProducto, vendedorPrincipal,index = [], indexCajas= [], contadorCajas=-1;
+let divSeparador, pElement, h6Element, cantidadProducto, vendedorPrincipal,index = [], indexCajas= [], indexVendedores = [], contadorCajas=-1;
 var imgElement, vendedor, cantidadesProductos;
 
 var productos = [
@@ -18,7 +18,7 @@ let vendedores = [
 ];
 
 
-function elementosCarrito(costo){
+function elementosCarrito(costo, venta){
     contadorCajas++;
         cantidadProducto = document.createElement("h6");
         cantidadProducto.classList.add("cantidadProducto");
@@ -40,7 +40,7 @@ function elementosCarrito(costo){
 
         pElement = document.createElement("p");
         pElement.classList.add("pScript");
-        pElement.innerHTML = `Atendido por: ${seleccionDelVendedor(costo)}`;
+        pElement.innerHTML = `Atendido por: ${seleccionDelVendedor(costo, venta)}`;
 
         ulElements.appendChild(liElement);
         liElement.appendChild(cantidadProducto);
@@ -50,9 +50,9 @@ function elementosCarrito(costo){
         divSeparador.appendChild(pElement);
 }
 
-function seleccionDelVendedor(costo){
+function seleccionDelVendedor(costo, venta){
     while(true){
-        vendedor= prompt("Por favor, coloque el número de quien fue el que lo atendio \n 1. Juana \n 2. Pedro");
+        vendedor = prompt("Por favor, coloque el número de quien fue el que lo atendio \n 1. Juana \n 2. Pedro");
         if(vendedor === "1"){
             vendedores[0].dineroObtenido1 += costo;
             return "Juana"
@@ -68,22 +68,32 @@ function seleccionDelVendedor(costo){
 function ponerProductosEnCarrito(nameBtn){
     if(nameBtn === "btn1"){
         if(productos[0].carrito1 === false){
+            index[0] = 1;
             elementosCarrito(productos[0].costo1);
             imgElement.src = "https://res.cloudinary.com/beleza-na-web/image/upload/w_1500,f_auto,fl_progressive,q_auto:eco,w_1800,c_limit/e_trim/v1/imagens/product/7363/07375cbe-d873-4ff3-88c1-ae241419da4c-acqua-di-gioia-giorgio-armani-eau-de-parfum-perfume-feminino-100ml.png";
             h6Element.innerHTML=`${productos[0].nombreProducto1} $${productos[0].costo1}`;
             cantidadesProductos = document.querySelectorAll(".cantidadProducto");
-            index[0] = 1;
             productos[0].carrito1 = true;
             indexCajas[0] = contadorCajas;
+            indexVendedores[0] = vendedor;
+
+            if(indexVendedores[0] === "1"){
+                vendedores[0].ventaDeAqua1 = index[0];
+            }else if(indexVendedores[0] === "2"){
+                vendedores[1].ventaDeAqua2 = index[0];
+            }
+
         }else if(productos[0].carrito1 ===true){
             index[0]++;
-            if(vendedor === "1"){
+
+            if(indexVendedores[0] === "1"){
                 vendedores[0].dineroObtenido1 += productos[0].costo1;
                 vendedores[0].ventaDeAqua1 = index[0];
-            }else if(vendedor === "2"){
+            }else if(indexVendedores[0] === "2"){
                 vendedores[1].dineroObtenido2 += productos[0].costo1;
                 vendedores[1].ventaDeAqua2 = index[0];
             }
+
         }
         cantidadesProductos[indexCajas[0]].innerHTML = `${index[0]}`;
     }else if(nameBtn === "btn2"){
@@ -95,15 +105,25 @@ function ponerProductosEnCarrito(nameBtn){
             index[1] = 1;
             productos[1].carrito2 = true;
             indexCajas[1] = contadorCajas;
+            indexVendedores[1] = vendedor;
+
+            if(indexVendedores[1] === "1"){
+                vendedores[0].ventaDeEmocion1 = index[1];
+            }else if(indexVendedores[1] === "2"){
+                vendedores[1].ventaDeEmocion2 = index[1];
+            }
+
         }else if(productos[1].carrito2 === true){
             index[1]++;
-            if(vendedor === "1"){
+
+            if(indexVendedores[1] === "1"){
                 vendedores[0].dineroObtenido1 += productos[1].costo2;
                 vendedores[0].ventaDeEmocion1 = index[1];
-            }else if(vendedor === "2"){
+            }else if(indexVendedores[1] === "2"){
                 vendedores[1].dineroObtenido2 += productos[1].costo2;
                 vendedores[1].ventaDeEmocion2 = index[1];
             }
+
         }
         cantidadesProductos[indexCajas[1]].innerHTML = `${index[1]}`;
     }else if(nameBtn === "btn3"){
@@ -115,12 +135,21 @@ function ponerProductosEnCarrito(nameBtn){
             productos[2].carrito3 = true;
             index[2] = 1;
             indexCajas[2] = contadorCajas;
+            indexVendedores[2] = vendedor;
+
+            if(indexVendedores[2] === "1"){
+                vendedores[0].ventaDeAlegria1 = index[2];
+            }else if(indexVendedores[2] === "2"){
+                vendedores[1].ventaDeAlegria2 = index[2];
+            }
+
         }else if(productos[2].carrito3 === true){
             index[2]++;
-            if(vendedor === "1"){
+
+            if(indexVendedores[2] === "1"){
                 vendedores[0].dineroObtenido1 += productos[2].costo3;
                 vendedores[0].ventaDeAlegria1 = index[2];
-            }else if(vendedor === "2"){
+            }else if(indexVendedores[2] === "2"){
                 vendedores[1].dineroObtenido2 += productos[2].costo3;
                 vendedores[1].ventaDeAlegria2 = index[2];
             }
@@ -135,12 +164,21 @@ function ponerProductosEnCarrito(nameBtn){
             productos[3].carrito4 = true;
             index[3] = 1;
             indexCajas[3] = contadorCajas;
+            indexVendedores[3] = vendedor;
+
+            if(indexVendedores[3] === "1"){
+                vendedores[0].ventaDeFrescura1 = index[3];
+            }else if(indexVendedores[3] === "2"){
+                vendedores[1].ventaDeFrescura2 = index[3];
+            }
+
         }else if(productos[3].carrito4 === true){
             index[3]++;
-            if(vendedor === "1"){
+
+            if(indexVendedores[3] === "1"){
                 vendedores[0].dineroObtenido1 += productos[3].costo4;
                 vendedores[0].ventaDeFrescura1 = index[3];
-            }else if(vendedor === "2"){
+            }else if(indexVendedores[3] === "2"){
                 vendedores[1].dineroObtenido2 += productos[3].costo4;
                 vendedores[1].ventaDeFrescura2 = index[3];
             }
@@ -157,16 +195,21 @@ for(i=0; i<=3; i++){
 }
 
 btnPagar.addEventListener("click" , () =>{
+    alert(vendedores[0].ventaDeAqua1);
+    alert(vendedores[1].ventaDeEmocion2);
     vendedores[0].productosVendidos1 = vendedores[0].ventaDeAlegria1 + vendedores[0].ventaDeAqua1 + vendedores[0].ventaDeEmocion1 + vendedores[0].ventaDeFrescura1;
     vendedores[1].productosVendidos2 = vendedores[1].ventaDeAlegria2 + vendedores[1].ventaDeAqua2 + vendedores[1].ventaDeEmocion2 + vendedores[1].ventaDeFrescura2;
+
     if(vendedores[0].productosVendidos1 > vendedores[1].productosVendidos2){
-        vendedorPrincipal = `${vendedores[0].personal1} con ${vendedores[0].productosVendidos1} productos`;;
+        vendedorPrincipal = `${vendedores[0].personal1} con ${vendedores[0].productosVendidos1} productos, mientras que el secundario fue: ${vendedores[1].personal2} con ${vendedores[1].productosVendidos2} productos`;
+
     }else if(vendedores[0].productosVendidos1 < vendedores[1].productosVendidos2){
-        vendedorPrincipal = `${vendedores[1].personal2} con ${vendedores[1].productosVendidos2} productos`;
+        vendedorPrincipal = `${vendedores[1].personal2} con ${vendedores[1].productosVendidos2} productos, mientras que el secundario fue: ${vendedores[0].personal1} con ${vendedores[0].productosVendidos1} productos`;
+
     }else{
         vendedorPrincipal = `los 2, la vendedora ${vendedores[0].personal1} y el vendedor ${vendedores[1].personal2} fueron sus principales vendedores con ${vendedores[0].productosVendidos1} productos`;
+
     }
 
-    console.log(`El total a pagar es: $${vendedores[0].dineroObtenido1 + vendedores[1].dineroObtenido2} uds \n Con Juana gasto: $${vendedores[0].dineroObtenido1} uds \n Con Pedro gasto: $${vendedores[1].dineroObtenido2} uds \n Su vendedor principal fue: ${vendedorPrincipal}`);
-    alert(`El total a pagar es: $${vendedores[0].dineroObtenido1 + vendedores[1].dineroObtenido2} uds \n Su vendedor principal fue: ${vendedorPrincipal}`);
+    alert(`El total a pagar es: $${vendedores[0].dineroObtenido1 + vendedores[1].dineroObtenido2} uds \nSu vendedor principal fue: ${vendedorPrincipal}`);
 });
